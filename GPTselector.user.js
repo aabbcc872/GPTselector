7d0c19c72a6a1a5950191234090ef82a26daf4ee
dxp <2220328339@qq.com>
2023-03-10 16:36:36 +0800

    Create GPTselector.user.js

---
+++ GPTselector.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         GPTselector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       ziyunhui
+// @match        https://www.tampermonkey.net/faq.php?ext=dhdg&locale=zh&version=4.18.1
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
